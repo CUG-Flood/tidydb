802130dc28e43701f881bc7aeef41ee861588e60
Dongdong Kong <kongdd@users.noreply.github.com>
2022-09-07 16:42:54 +0800

    mv curlR functions to here

---
+++ R/reexports.R
@@ -0,0 +1,27 @@
+#' @importFrom lubridate year month day hour
+#' @export 
+lubridate::year
+
+#' @export
+lubridate::month
+
+#' @export
+lubridate::day
+
+#' @export
+lubridate::hour
+
+
+#' @importFrom data.table data.table fread fwrite
+#' @export
+data.table::fread
+
+#' @export
+data.table::fwrite
+
+#' @export
+data.table::data.table
+
+#' @importFrom stringr str_extract
+#' @export
+stringr::str_extract
--- R/tidydb-package.R
+++ R/tidydb-package.R
@@ -1,7 +1,16 @@
-#' @import magrittr
+#' @import RSQLite
+#' @import magrittr glue
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+.onLoad <- function(libname, pkgname) {
+  if (getRversion() >= "2.15.1") {
+    utils::globalVariables(
+      c(".", ".SD", ".N", "timestr", "timenum", "timeinfo")
+    )
+  }
+}
